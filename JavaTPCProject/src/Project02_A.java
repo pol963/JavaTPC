77cdcd8fb89af2b63aa5dbd6c7b248ef4510d990
pol963 <pol963@naver.com>
2022-04-17 05:56:41 +0900

    22_04_17_02

--- JavaTPCProject/src/Project02_A.java
+++ JavaTPCProject/src/Project02_A.java
@@ -1,5 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Project02_A {
@@ -9,6 +10,7 @@ public class Project02_A {
 		
 		//파싱할 웹 페이지 url.
 		String url ="https://sports.news.naver.com/wfootball/index";
+		//jsoup로 얻어온 html의 모든것을 담는 전체문서.
 		Document doc = null;
 		
 		//예외처리.
@@ -23,11 +25,43 @@ public class Project02_A {
 		
 		//doc로부터 각 요소 가져와 보기.
 		
+		//Elements -> 가져온html을 담는 클래스. 
 		//웹 페이지에 목표한 부분의 div태그안의 home_news부분 선택.
 		Elements elements = doc.select("div.home_news");
 		
+		//h2태그 -> 제목 가져와보기.
+		String title = elements.select("h2").text().substring(0,4);
+		System.out.println("============================");
+		System.out.println(title);
 		
+		for(Element el : elements.select("li")) {
+			System.out.println(el.text());
+		}
+		System.out.println("============================");
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
