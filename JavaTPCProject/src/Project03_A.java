ca509a8f9dfc7065d125e152d484ade108bc87c3
pol963 <pol963@naver.com>
2022-04-20 00:36:35 +0900

    22_04_20_01

--- JavaTPCProject/src/Project03_A.java
+++ JavaTPCProject/src/Project03_A.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.util.*;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -36,20 +37,41 @@ public class Project03_A {
 			//rows의 데이터에 첫번째 row는 title...등 실질적인 데이터가 아니기에 next메서드로 로우 하나를 건너뜀.
 			rows.next();
 			
+			//셀의 데이터를 가져왔다면 arrayList에 넣기위해 -> vo구조로 셀의 데이터를 모아서 arrayList에 넣기위해
+			//임시 배열의 형태로 셀의 데이터를 받아주기.
+			String[] imsi = new String[5];
 			//반복문을 사용하여 rows.hasNext메서드를 이용해서 row가 있다면 next()리턴으로 row에 저장.
-			while(rows.hasNext()) {
+			while(rows.hasNext()) {//엑셀의 row를 읽어들이기 위한 반복문.
 				HSSFRow row = (HSSFRow) rows.next(); 
 				
 				//cell또한 가져와야 핸들링 할 수있습니다. 반복자를 통해 cell들을 가져와서 cells에 담기.
 				Iterator<Cell> cells = row.cellIterator();
 				
+				int i = 0; //imsi에 데이터를 하나씩넣어주기 위한 변수. 반복문.
+				while(cells.hasNext()) {
+					HSSFCell cell = (HSSFCell) cells.next();
+					imsi[i] = cell.toString(); //cell로 하나씩 가져온 cell데이터를 배열에 넣어주기.
+					i++;
+				}
+				
+				//묶기->vo 담기->list
+				//임시배열의 cell값을(5개의 데이터 반복문의 의해 범위 지정) ExcelVO로 묶어주기.
+				ExcelVO vo = new ExcelVO(imsi[0],imsi[1],imsi[2],imsi[3],imsi[4]);
+				data.add(vo);
+				
 			}
 			
-			
+			showExcelData(data);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public static void showExcelData(List<ExcelVO> data) {
+		for(ExcelVO vo : data ) {
+			System.out.println(vo);
+		}
 	}
 
 }
